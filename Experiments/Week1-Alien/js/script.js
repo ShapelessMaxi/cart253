a4d917913ad9178de45347cae4cf4ad839855996
ShapelessMaxi <maxime.perreault@hotmail.fr>
2021-09-13 10:32:29 -0400

    Week1-Alien: drew an alien

--- Experiments/Week1-Alien/js/script.js
+++ Experiments/Week1-Alien/js/script.js
@@ -1,9 +1,8 @@
 /**
-Title of Project
-Author Name
+Week 1 - Alien
+Maxime Perreault
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Activity 2: Draw an alien
 */
 
 "use strict";
@@ -13,15 +12,40 @@ author, and this description to match your project!
 Description of preload
 */
 function preload() {
-
 }
 
 
 /**
-Description of setup
+Draw an alien
 */
 function setup() {
-  background(255, 255, 55);
+  createCanvas(640, 480);
+  background(255, 181, 255);
+
+  //Draw the body
+  noStroke();
+  fill(125);
+  ellipse(320, 480, 300, 200);
+
+  //Draw the head
+  fill(180);
+  ellipse(320, 240, 250, 400);
+
+  //Draw the eyes
+  fill(0);
+  ellipse(260, 242, 55, 190);
+  ellipse(380, 242, 55, 190);
+
+  //Draw the nostrils
+  ellipse(325, 335, 6, 10);
+  ellipse(315, 335, 6, 10);
+
+  //Draw the mouth
+  stroke(200, 15, 15);
+  strokeWeight(9);
+  rectMode(CENTER);
+  rect(320, 360, 155, 20);
+
 }
 
 
