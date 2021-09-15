da02dd4d50f4cb0b2b75bc9f975fd5862d6901a5
ShapelessMaxi <maxime.perreault@hotmail.fr>
2021-09-15 11:22:08 -0400

    variable objects

--- experiments/variables/js/script.js
+++ experiments/variables/js/script.js
@@ -1,33 +1,30 @@
 /**
 everything has a name!
 Maxime Perreault
-
-
 */
 
 // setup of variables
 let backgroundRgba = [150, 220, 150, 10];
-let circleSize = 50;
-let circleSpeed = 0.15;
-let circleX = 75;
-let circleY = 75;
-/**
-canvas
-*/
+let circle = {
+  size: 50,
+  speed: 0.15,
+  x: 75,
+  y:75,
+};
+
+// canvas
 function setup() {
   createCanvas(500, 500);
 }
 
-/**
-trying variables stuff here
-*/
+// drawing stuff
 function draw() {
   background(backgroundRgba);
   // background color change
   // background_rgba += 1
 
-  circle(circleX, circleY, circleSize);
-  circleX += circleSpeed;
+  ellipse(circle.x, circle.y, circle.size);
+  circle.x += circle.speed;
 
   //
 }
