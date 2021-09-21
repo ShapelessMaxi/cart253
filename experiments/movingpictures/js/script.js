0cfc6da87d19a64d6eba38f6271fd34cc6b9a382
ShapelessMaxi <maxime.perreault@hotmail.fr>
2021-09-21 11:15:19 -0400

    A2: finished activities

--- experiments/movingpictures/js/script.js
+++ experiments/movingpictures/js/script.js
@@ -1,33 +1,67 @@
 /**
-Title of Project
-Author Name
+Moving pictures activity
+Maxime Perreault
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+following the mving picture activity video.
 */
 
-"use strict";
+let bg = {
+  r: 0,
+  g: 0,
+  b: 0,
+};
 
+let circleA = {
+  x: 0,
+  y: 250,
+  size: 100,
+  speed: 2,
+  growR: 2,
+  fill: 255,
+  alpha: 200,
+};
+
+let circleB = {
+  x: 500,
+  y: 250,
+  size: 80,
+  relativeRatio: 0.75,
+  speed: -2,
+  fill: 255,
+  alpha: 200,
+};
 
 /**
-Description of preload
-*/
-function preload() {
-
-}
-
-
-/**
-Description of setup
+Setup
 */
 function setup() {
-  background(255, 255, 55);
+  createCanvas(500, 500);
+  noStroke();
+  circleA.y = height/2;
+  circleB.x = width;
+  circleB.y = height/2;
 }
 
-
 /**
-Description of draw()
+Drawing the moving stuff
 */
 function draw() {
+  // background
+  background(bg.r, bg.g, bg.b);
+  bg.r = map(circleA.size, 100, width, 0, 255);
 
+  // left circle (A)
+  circleA.x += circleA.speed;
+  circleA.x = constrain(circleA.x, 0, width/2);
+  circleA.size += circleA.growR;
+  circleA.size = constrain(circleA.size, 0, width);
+  fill(circleA.fill, circleA.alpha);
+  ellipse(circleA.x, circleA.y, circleA.size);
+
+  // right circle (B)
+  circleB.x += circleB.speed;
+  circleB.x = constrain(circleB.x, width/2, width)
+  circleB.size = circleA.size * circleB.relativeRatio;
+  fill(circleB.fill, circleB.alpha);
+  ellipse(circleB.x, circleB.y, circleB.size);
 }
