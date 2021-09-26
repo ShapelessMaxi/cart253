334180243af48ec9e3336575ad8defaca03a2012
ShapelessMaxi <maxime.perreault@hotmail.fr>
2021-09-26 15:28:58 -0400

    finished covid activity

--- activities/avoidcovid/js/script.js
+++ activities/avoidcovid/js/script.js
@@ -1,33 +1,73 @@
 /**
-Title of Project
-Author Name
-
-This is a template. You must fill in the title,
-author, and this description to match your project!
+dodging covid activity
+Maxime Perreault
 */
 
 "use strict";
 
+let covid = {
+  x: 0,
+  y: 250,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speed: 25,
+  fill: {
+    r: 180,
+    g: 20,
+    b: 20,
+  },
+};
 
-/**
-Description of preload
-*/
-function preload() {
+let victim = {
+  x: 250,
+  y: 250,
+  size: 100,
+  fill: 255,
+};
 
-}
+let staticAmount = 2500;
 
-
-/**
-Description of setup
-*/
 function setup() {
-  background(255, 255, 55);
+  createCanvas(windowWidth, windowHeight);
+  noCursor();
+
+  covid.y = random(0, height);
+  covid.vx = covid.speed;
 }
 
-
-/**
-Description of draw()
-*/
 function draw() {
+  background(0);
 
+  for (let i = 0; i < staticAmount; i++) {
+    let x = random(0, width);
+    let y = random(0, height);
+    stroke(200);
+    point(x, y);
+  }
+
+  covid.x += covid.vx;
+  covid.y += covid.vy;
+
+  victim.x = mouseX;
+  victim.y = mouseY;
+
+  if (covid.x > width) {
+    covid.x = 0;
+    covid.y = random(0, height);
+  }
+
+  let d = dist(victim.x, victim.y, covid.x, covid.y);
+  if (d < victim.size / 2 + covid.size / 2) {
+    victim.fill = 15;
+    noLoop();
+  }
+
+  noStroke();
+  fill(victim.fill);
+  rectMode(CENTER);
+  rect(victim.x, victim.y, victim.size);
+
+  fill(covid.fill.r, covid.fill.g, covid.fill.b);
+  ellipse(covid.x, covid.y, covid.size);
 }
