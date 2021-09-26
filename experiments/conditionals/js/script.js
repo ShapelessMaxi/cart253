0675767f9ddcb056e7d7b6b917d48ff31799527e
ShapelessMaxi <maxime.perreault@hotmail.fr>
2021-09-26 14:57:33 -0400

    continued conditionals, started covid activity

--- experiments/conditionals/js/script.js
+++ experiments/conditionals/js/script.js
@@ -1,12 +1,28 @@
 /**
-conditionals tutorial
+Conditionals tutorial
 Maxime Perreault
 */
 
 "use strict";
 
+let angle = 0;
+let rectScale = 0;
+
 function setup() {
-  background(255, 255, 55);
+  createCanvas(500, 500);
+  background(100, 100, 200);
 }
 
-function draw() {}
+function draw() {
+  push();
+  fill(200, 20, 20, 5);
+  rectMode(CENTER);
+  translate(width / 2, height / 2);
+  rotate(angle);
+  scale(rectScale);
+  rect(0, 0, 100, 100);
+  pop();
+
+  angle += 0.05;
+  rectScale += 0.008;
+}
