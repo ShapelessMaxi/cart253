c0909940692eec552dd67e64cb955a8148871574
ShapelessMaxi <maxime.perreault@hotmail.fr>
2021-09-28 12:19:37 -0400

    E2: started love simulation

---
+++ exercises/love-actually/js/script.js
@@ -0,0 +1,37 @@
+/**
+Love, actually
+Maxime Perreault
+
+We're often looking for love anywhere we can find it... because we feel like we need it!
+The problem is that the amount of compatible matches are limited. I know, there's plenty
+of fish in the sea... but let's be realistic here. In this simulation, I want to explore
+the idea that not every 'love interest' is a good one. I also want to add the idea that
+there isn't just one perfect match.
+
+As the player, you control a circle that starts grey. There's another circle at the start,
+it's your first love interest. Every time yo click the left-mouse button, you change color randomly.
+Every time you click, another randomly colored circle appears, but the other circles shrink in size.
+There will be a maximum nuber of circles on the screen, the smaller circles get eliminated.
+You have to find the color to match the another circle.
+
+Easter egg: if there is more than one matching circle, you can go around and love them all!
+*/
+
+"use strict";
+
+/**
+Description of preload
+*/
+function preload() {}
+
+/**
+Description of setup
+*/
+function setup() {
+  background(255, 255, 55);
+}
+
+/**
+Description of draw()
+*/
+function draw() {}
