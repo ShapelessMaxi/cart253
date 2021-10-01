71d21ee3a94dd375d5304d8f974b45b0cda9f409
ShapelessMaxi <maxime.perreault@hotmail.fr>
2021-10-01 17:48:46 -0400

    turorial week5: finished automated movement vid

--- experiments/functions/js/script.js
+++ experiments/functions/js/script.js
@@ -1,26 +1,39 @@
-let bg = 0;
+let circle = {
+  x: 100,
+  y: 300,
+  vx: 1,
+  vy: 1,
+  size: 100,
+  speed: 10,
+};
 
 function setup() {
   createCanvas(500, 500);
 }
 
 function draw() {
-  background(bg);
+  background(0);
 
-  textAlign(CENTER, CENTER);
-  textSize(64);
-  fill(255);
-  text(key, width / 2, height / 2);
+  // let change = random();
+  // if (change < 0.05) {
+  //   circle.vx = random(-circle.speed, circle.speed);
+  //   circle.vy = random(-circle.speed, circle.speed);
+  // }
 
-  if (keyIsDown(65)) {
-    rect(200, 200, 200, 200);
+  let dx = circle.x - mouseX;
+  let dy = circle.y - mouseY;
+  if (dx < 0) {
+    circle.vx = circle.speed;
+  } else if (dx > 0) {
+    circle.vx = -circle.speed;
   }
-}
-
-function keyPressed() {
-  if (keyCode === 32) {
-    bg = 127;
-  } else {
-    bg = 0;
+  if (dy < 0) {
+    circle.vy = circle.speed;
+  } else if (dy > 0) {
+    circle.vy = -circle.speed;
   }
+
+  circle.x += circle.vx;
+  circle.y += circle.vy;
+  ellipse(circle.x, circle.y, circle.size);
 }
