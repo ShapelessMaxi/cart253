90fdd6df2c4093d93f770037633839431e139674
ShapelessMaxi <maxime.perreault@hotmail.fr>
2021-10-29 15:56:34 -0400

    completed 9.2 tutorial

--- experiments/inheritance and polymorphism/js/Car.js
+++ experiments/inheritance and polymorphism/js/Car.js
@@ -4,6 +4,29 @@ class Car extends Vehicle {
     this.w = 50;
     this.h = 20;
     this.vx = 5;
+    this.drunkenness = 0.2;
+  }
+
+  move() {
+    this.veer();
+    super.move();
+  }
+
+  veer() {
+    let r = random();
+    if (r < this.drunkenness) {
+      this.vy = random(-5, 5);
+    }
+  }
+
+  wrap() {
+    super.wrap();
+
+    if (this.y > height) {
+      this.y -= height;
+    } else if (this.y < 0) {
+      this.y += height;
+    }
   }
 
   display() {
