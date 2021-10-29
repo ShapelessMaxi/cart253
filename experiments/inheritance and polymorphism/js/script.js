75e414239658cb35b9b1ebff2ccc6cbe0da7fbc2
ShapelessMaxi <maxime.perreault@hotmail.fr>
2021-10-29 16:10:04 -0400

    completed 9.3 tutorial

--- experiments/inheritance and polymorphism/js/script.js
+++ experiments/inheritance and polymorphism/js/script.js
@@ -1,9 +1,7 @@
 "use strict";
 
-let cars = [];
+let vehicles = [];
 let numCars = 20;
-
-let motos = [];
 let numMoto = 20;
 
 function setup() {
@@ -13,31 +11,24 @@ function setup() {
     let x = random(0, width);
     let y = random(0, height);
     let car = new Car(x, y);
-    cars.push(car);
+    vehicles.push(car);
   }
 
   for (let i = 0; i < numMoto; i++) {
     let x = random(0, width);
     let y = random(0, height);
     let moto = new Moto(x, y);
-    cars.push(moto);
+    vehicles.push(moto);
   }
 }
 
 function draw() {
   background(0);
 
-  for (let i = 0; i < cars.length; i++) {
-    let currentCar = cars[i];
-    currentCar.move();
-    currentCar.wrap();
-    currentCar.display();
-  }
-
-  for (let i = 0; i < motos.length; i++) {
-    let currentMoto = motos[i];
-    currentMoto.move();
-    currentMoto.wrap();
-    currentMoto.display();
+  for (let i = 0; i < vehicles.length; i++) {
+    let currentVehicle = vehicles[i];
+    currentVehicle.move();
+    currentVehicle.wrap();
+    currentVehicle.display();
   }
 }
